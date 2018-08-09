3d7446a76a8d989f2a1e222d27f530d6fe3fea48
laincarl <1047972853@qq.com>
2018-08-09 09:06:50 +0800

    [ADD初始化]

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <XButton @click="increment">+</XButton>
+      <XButton @click="decrement">-</XButton>
+    </p>
+
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from '@/store/store.js'
+import { XButton } from 'vux'
+export default {
+  components: {
+    XButton
+  },
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
